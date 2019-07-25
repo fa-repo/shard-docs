825d9c67400ff4cf7bc9e795ebd65eb8d9e3665b
snoke <prjctnxt@gmail.com>
2019-07-25 02:28:05 +0100

    Updates

---
+++ precache-manifest.33bcddac583f357d4c844564542469e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31ee22271fa428da1d9b860c9e497b1",
+    "url": "/shard-docs/index.html"
+  },
+  {
+    "revision": "ba9660690719f832f08a",
+    "url": "/shard-docs/static/css/main.749b880b.chunk.css"
+  },
+  {
+    "revision": "848df25737cbfb8fdb09",
+    "url": "/shard-docs/static/js/2.faaa04b6.chunk.js"
+  },
+  {
+    "revision": "ba9660690719f832f08a",
+    "url": "/shard-docs/static/js/main.664829d4.chunk.js"
+  },
+  {
+    "revision": "fed83b11e8bea9ec1b57",
+    "url": "/shard-docs/static/js/runtime~main.58f0a929.js"
+  }
+]);
